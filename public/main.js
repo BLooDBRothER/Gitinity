d623ccd55a61ee690a7bbfee738aa295aa35d91b
Jayashakthi28 <vishnu28.js@gmail.com>
2021-08-18 07:27:12 +0530

    finished typing effect

--- public/main.js
+++ public/main.js
@@ -8,4 +8,69 @@ function displayTime() {
     setTimeout("displayTime()",1000);
 }
 
+function sleep(fun,time){
+    setTimeout(fun,time);
+}
+function typer(currText,tag){
+    let index=0;
+    let str='';
+    let timeCount=0;
+    for(let i=0;i<currText.length;i++){
+        setTimeout(()=>{
+        str+=currText[i];
+        tag.innerHTML=str;
+        },60*(i+1));
+    }
+}
+
+let prevLength=100;
+
+function typerDriver(data){
+    const tags=[data.querySelector('p'),data.querySelector('pre')];
+    tags.forEach((textD,idx)=>{
+        if(!textD) return;
+        if(idx===0){
+            let text=textD.textContent;
+            textD.innerHTML='';
+            setTimeout(()=>{typer(text,textD)},60);
+        }
+        else{
+            let text=textD.textContent;
+            textD.innerHTML='';
+            setTimeout(()=>{typer(text,textD)},(idx+1)*400);
+        }
+    })
+}
+
+const terminal_command_divs=document.querySelectorAll('.terminal-commands');
+
+function timeCalc(data,i){
+    let time=60;
+    const tags=[data.querySelector('p'),data.querySelector('pre')];
+    tags.forEach((txt,idx)=>{
+        if(txt)
+        time+=txt.textContent.length*70;
+    });
+    return time;
+}
+
+let prevTime=60;
+
+terminal_command_divs.forEach((data,index)=>{
+    if(index===0){
+        setTimeout(()=>{
+            data.classList.remove('none');
+            typerDriver(data);
+        },60);  
+        prevTime=60; 
+    }
+    else{
+        prevTime+=timeCalc(terminal_command_divs[index-1],index);
+        console.log(prevTime)
+        setTimeout(()=>{
+            data.classList.remove('none');
+            typerDriver(data);
+        },prevTime);      
+    }
+})
 displayTime();
