5434e79770869c4e2b64efad139f7c92af22c84f
BLooDBRothER <computertechnology446@gmail.com>
2021-08-20 20:18:53 +0530

    Update fetech in themes.js

--- public/js/themes.js
+++ public/js/themes.js
@@ -2,6 +2,15 @@ const theme_btn = document.querySelector(".head__theme");
 const themes = document.querySelector(".themes");
 let selected = "", preveiousSelected = "";
 
+let path = "../../themes.json"
+
+async function fetchTheme(){
+    let response = await fetch("../themes.json");
+    let result = await response.json();
+    console.log(result.theme);
+}
+fetchTheme();
+
 theme_btn.addEventListener("click", (e) => {
     themes.classList.toggle("none");
 });
