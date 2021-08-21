599a4a2b45cf61b3bc0363e635af581970585aa1
Savage Sparky <akilan73210@gmail.com>
2021-08-21 13:40:06 +0530

    added click func to side_title

--- public/js/documentation.js
+++ public/js/documentation.js
@@ -5,3 +5,13 @@ nav_toggler.addEventListener("click", function() {
     document.querySelector('.side_nav').style.left = '-250px';
     document.querySelector('.document-section').style.paddingLeft = 'unset';
 });
+
+side_title_toggler = document.querySelector('.side-title');
+side_title_toggler.addEventListener('click', () => {
+    if(side_title_toggler.classList.contains('toggle_side_title')){
+        side_title_toggler.classList.remove('toggle_side_title');
+    }
+    else{
+        side_title_toggler.classList.add('toggle_side_title');
+    }
+})
