5019c88044991a89ceb7be6e9285493f3fb6af10
Maheshkumar <callmeelprofessor@gmail.com>
2021-08-27 21:18:06 +0530

    Refactor Theme Creator

--- public/js/theme_creator.js
+++ public/js/theme_creator.js
@@ -1,4 +1,5 @@
 const themeCreator = document.querySelector(".theme-creator");
+const body = document.body;
 
 themeCreator.style.height = `${document.body.offsetHeight}px`;
 
@@ -6,7 +7,8 @@ function toggle() {
   themeCreator.classList.toggle("toggle-bg");
   document.querySelectorAll(".theme-creator *").forEach((child) => {
     console.log(child);
-    child.classList.toggle("none")
+    child.classList.toggle("none");
   });
+  body.classList.toggle("hide-overflow");
 }
 document.querySelector(".header__preview").addEventListener("click", toggle);
