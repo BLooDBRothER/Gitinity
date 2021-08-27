b7c8ebb1baef92ce0f36e8fc56fd4c2a5a40ebc1
Maheshkumar <callmeelprofessor@gmail.com>
2021-08-27 21:34:11 +0530

    Update theme creator to default

--- public/js/theme_creator.js
+++ public/js/theme_creator.js
@@ -2,11 +2,12 @@ const themeCreator = document.querySelector(".theme-creator");
 const body = document.body;
 
 themeCreator.style.height = `${document.body.offsetHeight}px`;
-
 function toggle() {
+  document.querySelectorAll("input").forEach((i) => {
+    document.documentElement.style.setProperty(`--${i.id}`, i.value);
+  });
   themeCreator.classList.toggle("toggle-bg");
   document.querySelectorAll(".theme-creator *").forEach((child) => {
-    console.log(child);
     child.classList.toggle("none");
   });
   body.classList.toggle("hide-overflow");
