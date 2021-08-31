7ef1e3d93330ad0a6fb05d256d8c9ed32a406755
BLooDBRothER <computertechnology446@gmail.com>
2021-08-31 13:17:34 +0530

    Add copy functionality

--- public/js/documentation.js
+++ public/js/documentation.js
@@ -5,3 +5,23 @@ nav_toggler.addEventListener("click", function() {
     document.querySelector('.side_nav').classList.toggle('toggle_nav');
     document.querySelector('.document-section').classList.toggle('document_section_toggle');
 });
+
+// copy js
+
+const copyIC = document.querySelectorAll(".code-div svg");
+
+function copy(e){
+    let copyStr = document.querySelector(`code[data-name="${this.dataset.name}"]`).innerText;
+    navigator.clipboard.writeText(copyStr);
+    let msg = document.querySelector(".copy-msg");
+    msg.firstElementChild.innerText = copyStr;
+    msg.syle.top = `${document.querySelector("body").scrollHeight}px`;
+    msg.style.right = "0";
+    setTimeout((e) => {
+        msg.style.right = "-100%";
+    }, 2000);
+}
+
+copyIC.forEach(ic => {
+    ic.addEventListener("click", copy);
+})
